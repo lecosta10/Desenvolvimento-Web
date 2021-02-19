7b03639a892fca752623e0df6b87d8ad23b7987a
Leticia Vieira Costa <lecosta893@gmail.com>
2021-02-19 11:44:03 -0300

    Desenvolvimento Web

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+
+alert("Seja Bem vindo!");
+
+
+function funTroca(par_alt, par_src) {
+    
+    img_destaque.src = par_src;
+
+    img_destaque.alt = img_destaque.title = p_destaque.innerHTML = par_alt;
+
+
+
+}
